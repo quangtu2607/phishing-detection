ecbd216a67123ab5b76fb01d32e53ff714edb261
quangtu2607 <quangtu2607@gmail.com>
2021-09-29 17:48:29 +0700

    extract url feature

--- backend/mysite/phishing/views.py
+++ backend/mysite/phishing/views.py
@@ -5,6 +5,7 @@ import json
 from django.contrib.auth.models import User #####
 from django.http import JsonResponse , HttpResponse ####
 from django.views.decorators.csrf import csrf_exempt
+import whois
 
 def index(request):
     return HttpResponse("Hello, world")
@@ -14,8 +15,11 @@ def get_page(request):
     if request.method == "POST":
         body_unicode = request.body.decode('utf-8')
         body_data = json.loads(body_unicode)
-        print(body_data['content'])
-        print(body_data['url'])
+        # print(body_data['content'])
+        # print(body_data['url'])
+
+        w = whois.whois(body_data['url'])
+        print(w)
     # Any process that you want
     data = {
             # Data that you want to send to javascript functions
